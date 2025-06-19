9d2adceffba52edf492ddade81ae15392dbcdac5
mahsamymoud <112322191+mahsamymoud@users.noreply.github.com>
2025-06-19 19:26:14 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GroupSwapApp from './swapApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GroupSwapApp />
+  </React.StrictMode>
+);
